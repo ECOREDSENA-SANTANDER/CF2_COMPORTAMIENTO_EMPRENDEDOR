63a15de7ab19f66bef57b55560730a4f1a2fd18f
Lucenith Pinilla Moreno <167031042+LucenithGitHub@users.noreply.github.com>
2025-06-24 09:43:20 -0500

    Créditos, Material Complementario, Referencias

--- src/config/global.js
+++ src/config/global.js
@@ -118,21 +118,115 @@ export default {
   },
   complementario: [
     {
-      tema: 'Tema 1',
-      referencia: 'Texto',
-      tipo: 'Sitio web',
-      link: 'Link',
+      tema: 'Liderazgo, retos y estrategias para permanecer en el mercado.',
+      referencia: 'Coll, F. M. (2020). Coyuntura económica. Economipedia.com.',
+      tipo: 'Página Web',
+      link: 'https://economipedia.com/definiciones/coyuntura-economica.html',
+    },
+    {
+      tema: 'Metodología CANVAS del entorno.',
+      referencia:
+        'SENA. (2023, 15 de marzo). Modelo CANVAS para emprendedores.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=DQhdG8uRVCM',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: 'Competitividad',
+      significado:
+        'capacidad de una sociedad para enfrentar con éxito los desafíos de la economía global y los procesos de internacionalización, mediante la generación de ventajas competitivas sostenibles que impulsen simultáneamente el bienestar general. Implica competir de manera eficiente en los mercados, elevando los ingresos reales y fortaleciendo la productividad, la innovación y la apertura comercial.',
+    },
+    {
+      termino: 'Demanda',
+      significado:
+        'cantidad máxima de un bien o servicio que un individuo o grupo está dispuesto a adquirir a un precio determinado, durante un período específico.',
+    },
+    {
+      termino: 'Matriz',
+      significado:
+        'en administración, herramienta analítica que permite estudiar diversas variables (financieras, administrativas, macroeconómicas, entre otras) que pueden influir en el desempeño de una empresa, negocio o sector.',
+    },
+    {
+      termino: 'Mercado',
+      significado:
+        'conjunto de consumidores identificables, con necesidades específicas, que están dispuestos a intercambiar bienes o servicios en un entorno regido por la ley oferta y demanda.',
+    },
+    {
+      termino: 'Monopolio',
+      significado:
+        'situación económica en la que un único proveedor domina el mercado frente a múltiples compradores, lo que le otorga poder para fijar precios y limitar la competencia.',
+    },
+    {
+      termino: 'Modelo de negocio',
+      significado:
+        'estructura conceptual que define como una organización crea, entrega y captura valor para sus clientes y stakeholders.',
+    },
+    {
+      termino: 'Segmentación de mercado',
+      significado:
+        'proceso que consiste en dividir un mercado en grupos más pequeños con características comunes (como ubicación, personalidad o comportamiento) para diseñar estrategias diferenciadas.',
+    },
+    {
+      termino: 'Segmento del mercado',
+      significado:
+        'grupo específico de personas, organizaciones o entidades con necesidades y deseos similares, que pueden ser satisfechos con una oferta comercial específica.',
+    },
+    {
+      termino: 'Segmento seleccionado',
+      significado:
+        'subconjunto del mercado meta elegido por el emprendedor, compuesto por individuos con características homogéneas como edad, sexo, ocupación o localización.',
+    },
+    {
+      termino: 'Satisfacción del cliente',
+      significado:
+        'grado en que los productos o servicios cumplen o superan las expectativas del consumidor, influyendo en su lealtad y recompra.',
+    },
+    {
+      termino: 'Tendencia',
+      significado:
+        'patrón de comportamiento de fenómenos o procesos que se orientan en una dirección determinada a largo del tiempo, y que son útiles para análisis y planificación.',
+    },
+    {
+      termino: 'Target o público objetivo',
+      significado:
+        'grupo de consumidores a los que se dirige una estrategia de mercado, definida por criterios demográficos, geográficos, psicográficos o conductuales.',
+    },
+    {
+      termino: 'Transformación digital',
+      significado:
+        'integración de tecnologías digitales en todas las áreas de una empresa, con el objetivo de mejorar procesos, generar valor y adaptarse a cambios en el entorno.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Alfaro, J., González, C. & Pina, M. (2013). Economía de la empresa. McGraw Hill.',
+      link: '',
+    },
+    {
+      referencia:
+        'Centro de Innovación Pública Digital (s. f.). Manual para el análisis de tendencias.',
+      link: '',
+    },
+    {
+      referencia:
+        'Confecámaras. (2019). Informe de dinámica empresarial en Colombia. Año 2018.',
+      link: '',
+    },
+    {
+      referencia:
+        'Corma, F. (2018). El canvas de la innovación. Ediciones Díaz de Santos.',
+      link: '',
+    },
+    {
+      referencia:
+        'Max-Neef. (1998). Desarrollo a Escala Humana. Conceptos, aplicaciones y algunas reflexiones. Icaria Editorial. SENA. (2012). Ser empresario en seis pasos.',
+      link:
+        'https://repositorio.sena.edu.co/sitios/ser_empresario/serempresario1/index.html#',
+    },
+    {
+      referencia: 'Valencia, H. (2007). Diccionario de marketing. 3R editores.',
       link: '',
     },
   ],
@@ -156,9 +250,14 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: ' ',
-          cargo: ' ',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Vilma Lucía Perilla Méndez',
+          cargo: 'Experta y diseñadora instruccional ',
+          centro: 'Centro de Gestión Industrial - Regional Distrito Capital',
+        },
+        {
+          nombre: 'Sandra Paola Morales Páez',
+          cargo: 'Evaluadora instruccional',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
       ],
     },
@@ -166,18 +265,18 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: ' ',
-          cargo: 'Diseñador de contenidos',
+          nombre: 'Yazmin Rocio Figueroa Pacheco',
+          cargo: 'Diseñadora de contenidos',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
-          nombre: ' ',
+          nombre: 'Lucenith Pinilla Moreno',
           cargo: 'Desarrollador <i>full stack</i>',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
-          nombre: ' ',
-          cargo: 'Animador y productor audiovisual',
+          nombre: 'Maria Alejandra Vera Briceño',
+          cargo: 'Animadora y productora audiovisual',
           centro: 'Centro Agroturístico - Regional Santander',
         },
       ],
