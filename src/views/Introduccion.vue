a08212247c7e6879259a8992a691455ab212b48c
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-07-30 09:22:06 -0500

    avance

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -20,8 +20,9 @@
       .col-lg-10.col-md-12.col-sm-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-        figcaption Video. 
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/crbSrP1l4L0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        figcaption Video. Herramientas para detectar oportunidades de negocio
+
 
 </template>
 
