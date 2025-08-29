9099b006e55ed3dc0ba2961e19ebba0b87a70570
IngLeoCastellanos <ingleonardocastellanos@gmail.com>
2025-08-29 15:10:26 -0500

    Avance_CFA

--- src/config/global.js
+++ src/config/global.js
@@ -102,7 +102,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/CF2_DU.zip',
       },
       {
         icono: 'fas fa-download',
